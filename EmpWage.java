62368c53e1b3ea129f723be81ce5ff628c660df6
jeevan <jeevanjnayak@gmail.com>
2022-01-18 12:15:03 +0530

    full-time and part-time employee wages

---
+++ EmpWage.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class EmpFullWage1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner( System.in);
+        int Is_Present = 1;
+        int wage;
+        double EmpStatus = Math.floor(Math.random() * 10) % 2;
+        if (EmpStatus == Is_Present) {
+            System.out.print("Enter how many hours worked: ");
+            int hours = input.nextInt();
+            wage = hours * 20;
+            if (hours == 8)
+                System.out.println("full time wage is " + wage);
+            else if(hours < 8)
+                System.out.println("part time wage is " + wage);
+            else
+                System.out.println("invalid hour");
+        } else
+            System.out.println("Employee is absent");
+    }
+}
